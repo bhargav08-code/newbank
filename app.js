b9a4d7736828f6df9f537b12a28ef240337f9783
bhargav08-code <85948072+bhargav08-code@users.noreply.github.com>
2023-08-17 10:57:08 +0530

    Update app.js
    updated names

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ document.addEventListener("contextmenu", function (e) {
 });
 
 const account1 = {
-  owner: "Sahir Bhagat",
+  owner: "Bhargav Garge",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 const account2 = {
-  owner: "Ronit Tyade",
+  owner: "John Doe",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
